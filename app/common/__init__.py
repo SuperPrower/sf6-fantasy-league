3a3162e5d660f797af87687d86498a93d9943358
Sergei Koziakov <dya.eshshmai@gmail.com>
2026-02-04 13:45:23 +0100

    Improve appdata handling

--- app/common/__init__.py
+++ app/common/__init__.py
@@ -8,6 +8,12 @@ APP_NAME = "SF6FantasyLeague"
 
 def get_app_data_dir() -> Path:
     if sys.platform == "win32":
-        return Path(os.environ["APPDATA"]) / APP_NAME
+        if "APPDATA" in os.environ:
+            return Path(os.environ["APPDATA"]) / APP_NAME
+        else:
+            return Path.home() / "AppData" / "Roaming" / APP_NAME
     elif sys.platform == "linux":
-        return Path(os.environ["XDG_DATA_HOME"]) / APP_NAME
+        if "XDG_DATA_HOME" in os.environ:
+            return Path(os.environ["XDG_DATA_HOME"]) / APP_NAME
+        else:
+            return Path.home() / ".local" / "share" / APP_NAME
